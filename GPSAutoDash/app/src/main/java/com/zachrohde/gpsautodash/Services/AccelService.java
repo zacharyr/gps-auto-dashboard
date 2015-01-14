df0f90e46ddc145721869a93e6e1446db31c57ff
Zachary Rohde <zrohde463@gmail.com>
2015-01-13 22:58:06 -0600

    Added accel service, about to change gps service listener

--- GPSAutoDash/app/src/main/java/com/zachrohde/gpsautodash/Services/AccelService.java
+++ GPSAutoDash/app/src/main/java/com/zachrohde/gpsautodash/Services/AccelService.java
@@ -1,4 +1,77 @@
 package com.zachrohde.gpsautodash.Services;
 
-public class AccelService {
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.zachrohde.gpsautodash.R;
+
+public class AccelService implements SensorEventListener {
+    // Member fields.
+    private Activity mActivity;
+    private View mRootView;
+
+    // Accelerometer related.
+    private SensorManager mManager;
+    private Sensor mAccel;
+
+    // Three main progress bars.
+    private ProgressBar mProgressBRK;
+    private ProgressBar mProgressMain;
+    private ProgressBar mProgressPWR;
+
+    private TextView mAccelView;
+
+    public AccelService(Activity activity, View rootView) {
+        mActivity = activity;
+        mRootView = rootView;
+        mManager = (SensorManager) mActivity.getSystemService(Context.SENSOR_SERVICE);
+        mAccel = mManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+
+        mAccelView = (TextView) mRootView.findViewById(R.id.acceleration_value);
+
+        mProgressBRK = (ProgressBar) mRootView.findViewById(R.id.brk_progress_bar);
+        mProgressMain = (ProgressBar) mRootView.findViewById(R.id.main_progress_bar);
+        mProgressPWR = (ProgressBar) mRootView.findViewById(R.id.pwr_progress_bar);
+
+        startListener();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // In landscape mode:
+        // X is up(+) and down(-)
+        // Y is left(-) and right(+)
+        // Z is forward(+) and backward(-)
+        float zValue = event.values[2];
+
+        mAccelView.setText(Float.toString(zValue));
+
+        if (zValue < 1) {
+            mProgressMain.setProgress(0);
+        } else if (zValue > 5) {
+            mProgressMain.setProgress(50);
+        } else if (zValue > 10) {
+            mProgressMain.setProgress(100);
+        }
+
+        //System.out.println("zValue: " + zValue);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+
+    private void startListener() {
+        mManager.registerListener(this, mAccel, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    private void stopListener() {
+        mManager.unregisterListener(this);
+    }
 }
--- GPSAutoDash/app/src/main/java/com/zachrohde/gpsautodash/Services/GPSService.java
+++ GPSAutoDash/app/src/main/java/com/zachrohde/gpsautodash/Services/GPSService.java
@@ -1,12 +1,163 @@
 package com.zachrohde.gpsautodash.Services;
 
-import android.os.AsyncTask;
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class GPSService extends AsyncTask<Void, Void, Void> {
-    protected Void doInBackground(Void... params) {
-        return null;
+import com.zachrohde.gpsautodash.R;
+
+import java.math.BigDecimal;
+
+// TODO (URGENT): fix textview lookups
+
+public class GPSService {
+    // Member fields.
+    private Activity mActivity;
+    private View mRootView;
+
+    // For distance calculations.
+    private boolean mOldMeasurements = false;
+    private double mOldLatitude = 0.0;
+    private double mOldLongitude = 0.0;
+
+    // For distance calculations.
+    private double mDistanceTraveled = 0.0;
+
+    /**
+     * Using the GPS, GPSService updates all the views on the UI that display GPS data. GPSService
+     * runs on the main thread as it operates on an interrupt.
+     */
+    public GPSService(Activity activity, View rootView) {
+        mActivity = activity;
+        mRootView = rootView;
+
+        setupGPS();
     }
 
-    protected void onPostExecute(Long result) {
+    /**
+     * Setup the GPS's location change listener.
+     */
+    private void setupGPS() {
+        // Acquire a reference to the system Location Manager.
+        LocationManager locationManager = (LocationManager) mActivity.getSystemService(Context.LOCATION_SERVICE);
+
+        // Check to see if the GPS is enabled.
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Toast.makeText(mActivity, R.string.gps_disabled, Toast.LENGTH_LONG).show();
+            mActivity.finish();
+        }
+
+        // Define a listener that responds to location updates.
+        LocationListener locationListener = new LocationListener() {
+            /**
+             * Called when a new location is found by the location provider.
+             */
+            public void onLocationChanged(Location location) {
+                updateSpeed(location);
+                updateLatLongAlt(location);
+                updateDistance(location);
+                // updateAcceleration(location);
+            }
+
+            /**
+             * Called when the provider status changes.
+             */
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            /**
+             * Called when the provider is enabled by the user.
+             */
+            public void onProviderEnabled(String provider) {}
+
+            /**
+             * Called when the provider is disabled by the user.
+             */
+            public void onProviderDisabled(String provider) {}
+        };
+
+        // Register the listener with the GPS Provider Location Manager to receive GPS updates.
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER , 0, 0, locationListener);
+        // Register the listener with the Network Provider Location Manager to receive network updates.
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER , 0, 0, locationListener);
+    }
+
+    /**
+     * Update the speedometer with the new location data.
+     */
+    private void updateSpeed(Location location) {
+        // Set the speed.
+        if (location.hasSpeed()) {
+            float speed = location.getSpeed();
+
+            TextView speed_view = (TextView) mRootView.findViewById(R.id.speed_value);
+            speed_view.setText(Integer.toString((int) (speed * 2.23694)));
+        }
+    }
+
+    /**
+     * Update the lat/long/alt with the new location data.
+     */
+    private void updateLatLongAlt(Location location) {
+        // Set the latitude.
+        double latitude = location.getLatitude();
+
+        TextView lat_view = (TextView) mRootView.findViewById(R.id.latitude_value);
+        lat_view.setText(Double.toString(latitude));
+
+        // Set the longitude.
+        double longitude = location.getLongitude();
+
+        TextView long_view = (TextView) mRootView.findViewById(R.id.longitude_value);
+        long_view.setText(Double.toString(longitude));
+
+        // Set the altitude.
+        if (location.hasAltitude()) {
+            double altitude = location.getAltitude();
+
+            TextView alt_view = (TextView) mRootView.findViewById(R.id.altitude_value);
+            alt_view.setText(Integer.toString((int) (altitude * 3.28084)));
+        }
+    }
+
+    /**
+     * Update the lat/long/alt with the new location data and calculated lat, long, and alt.
+     */
+    private void updateDistance(Location location) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        // We only want to calculate the distance when we have high accuracy.
+        // TODO: let the user change this number
+        if (location.hasAccuracy() && location.getAccuracy() < 15.0) {
+            // Only calculate the distance when we have obtained at least one set of measurements.
+            if (!mOldMeasurements) {
+                mOldLatitude = latitude;
+                mOldLongitude = longitude;
+                mOldMeasurements = true;
+            } else {
+                // The distance in meters is the first element returned from distanceBetween().
+                float[] distance = new float[1];
+                Location.distanceBetween(mOldLatitude, mOldLongitude, latitude, longitude, distance);
+
+                mDistanceTraveled += distance[0];  // Update the running total of distance traveled.
+
+                // Round to the hundredth decimal place.
+                BigDecimal bd = new BigDecimal(Double.toString(mDistanceTraveled * 3.28084));
+                bd = bd.setScale(2, BigDecimal.ROUND_HALF_UP);
+
+                TextView dist_view = (TextView) mRootView.findViewById(R.id.distance_value);
+                dist_view.setText(Double.toString(bd.doubleValue()));
+
+                // Store the new measurements.
+                mOldLatitude = latitude;
+                mOldLongitude = longitude;
+            }
+        }
     }
 }
